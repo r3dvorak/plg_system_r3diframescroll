de14a6d95c392a2b379dac4c33724f9d08f4baeb
Richard Dvorak <github@r3d.de>
2026-06-22 20:57:38 +0200

    1.0.9: hard test onAfterRender injectio

--- 01_src/plg_system_r3diframescroll/r3diframescroll.php
+++ 01_src/plg_system_r3diframescroll/r3diframescroll.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     plg_system_r3diframescroll
- * @version     1.0.8
+ * @version     1.0.9
  * @author      Richard Dvorak <info@r3d.de>
  * @copyright   2026 Richard Dvorak
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -9,36 +9,37 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Application\CMSApplicationInterface;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Uri\Uri;
 
 class PlgSystemR3diframescroll extends CMSPlugin
 {
-    protected $autoloadLanguage = true;
-    private const HARD_TEST_VERSION = '1.0.8';
-
-    public function onBeforeCompileHead(): void
+    public function onAfterRender()
     {
-        $app = $this->getApplication();
+        $app = Factory::getApplication();
 
-        if (!$app instanceof CMSApplicationInterface || !$app->isClient('site')) {
+        if (!$app->isClient('site')) {
             return;
         }
 
-        $document = $app->getDocument();
+        $body = $app->getBody();
 
-        if (!method_exists($document, 'getType') || $document->getType() !== 'html') {
+        $marker = '<!-- R3D Iframe Scroll HARD TEST 1.0.9 -->';
+
+        if (strpos($body, $marker) !== false) {
             return;
         }
 
-        $document->addCustomTag('<!-- R3D Iframe Scroll HARD TEST 1.0.8 -->');
-        $document->addCustomTag('<!-- R3D Iframe Scroll HTML document confirmed -->');
+        $script = $marker . "\n"
+            . '<script src="' . Uri::root(true) . '/media/plg_system_r3diframescroll/js/r3diframescroll.js?v=1.0.9" defer></script>';
 
-        $document->addScript(
-            Uri::root(true) . '/media/plg_system_r3diframescroll/js/r3diframescroll.js?v=' . self::HARD_TEST_VERSION,
-            [],
-            ['defer' => true]
-        );
+        if (stripos($body, '</head>') !== false) {
+            $body = str_ireplace('</head>', $script . "\n</head>", $body);
+        } else {
+            $body .= "\n" . $script;
+        }
+
+        $app->setBody($body);
     }
 }
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,14 @@ All notable changes to this project will be documented in this file.
 
 The format is based on Keep a Changelog.
 
+## [1.0.9] - 2026-06-22
+
+### Changed
+
+- Switched the hard test bootstrap to `onAfterRender()` with direct HTML injection.
+- Replaced application access with `Factory::getApplication()` for the frontend hard test.
+- Removed document and parameter checks from the hard test to isolate frontend execution.
+
 ## [1.0.8] - 2026-06-22
 
 ### Changed
